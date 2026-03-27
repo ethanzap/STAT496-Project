509cb5235c709e1644bcaf44f5512d6df1a7892c
Bohan Yao <62229314+bohanyao-nlp@users.noreply.github.com>
2026-03-26 20:25:57 -0700

    Update launch_vllm.sh

--- launch_vllm.sh
+++ launch_vllm.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server --port 8000 --model /snow.core_llm.home/models/llama3.1/Meta-Llama-3.1-8B-Instruct --max-model-len 16384 --served-model-name llama &
-CUDA_VISIBLE_DEVICES=1 python -m vllm.entrypoints.openai.api_server --port 8001 --model /snow.core_llm.home/models/gemma/gemma-3-12b-it --max-model-len 16384 --language-model-only --served-model-name gemma &
+CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server --port 8000 --model Meta-Llama-3.1-8B-Instruct --max-model-len 16384 --served-model-name llama &
+CUDA_VISIBLE_DEVICES=1 python -m vllm.entrypoints.openai.api_server --port 8001 --model gemma-3-12b-it --max-model-len 16384 --language-model-only --served-model-name gemma &
